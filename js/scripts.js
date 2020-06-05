310dbb56c86f83dbd629eef452cc3b16e212e240
Sara Kane <kane.sarak@gmail.com>
2020-06-05 13:11:45 -0700

    Add functionality to show programming language result and change result based on answer to the first question

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,35 @@ $(document).ready(function() {
     const question2Input = $("input:radio[name=question2]:checked").val();
     const question3Input = $("input:radio[name=question3]:checked").val();
     const question4Input = $("input:radio[name=question4]:checked").val();
-    const question5Input = $("input:radio[name=question5]:checked").val(); 
+    const question5Input = $("input:radio[name=question5]:checked").val();
+
+    let languageResult;
+    if (question1Input === "money") {
+      languageResult = "JavaScript";
+    } 
+    else if (question1Input === "skill") {
+      languageResult = "Python";
+    } 
+    else if (question1Input === "idea") {
+      languageResult = "C#";
+    } 
+    else {
+      languageResult = "Python";
+    }
+
+    $("#result-python,#result-javascript,#result-csharp").hide();
+    if (languageResult == "JavaScript"){
+      $("#language-result").text("JavaScript");
+      $("#result-javascript").show();
+    }
+    else if (languageResult == "Python") {
+      $("#language-result").text("Python");
+      $("#result-python").show();
+    }
+    else if (languageResult == "C#") {
+      $("#language-result").text("C#");
+      $("#result-csharp").show();
+    }
+    $("#results").show();
   });
 });
