1b1b039b81b52ca786cb916dbbc39f8c33dd2680
Sara Kane <kane.sarak@gmail.com>
2020-06-05 11:32:18 -0700

    Add variables that get the values of the form inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-  
+  $("#questions").submit(function(event) {
+    event.preventDefault();
+    const question1Input = $("#question1").val();
+    const question2Input = $("input:radio[name=question2]:checked").val();
+    const question3Input = $("input:radio[name=question3]:checked").val();
+    const question4Input = $("input:radio[name=question4]:checked").val();
+    const question5Input = $("input:radio[name=question5]:checked").val(); 
+  });
 });
